da1a50baaaab0358317902be023c8151946081c4
Joel Wadagnolo <joelwadagnolo@yahoo.com>
2013-10-08 16:44:19 -0700

    Incorporating Lab5 (pipe Lab) information.  Added pipe.c, changed some files. TODO in ucode.c, within the int read_pipe() and int write_pipe() functions the sscanf doesnt want to work

---
+++ pipe.c
@@ -0,0 +1,92 @@
+#include "type.h"
+
+char *MODE[ ] = {"READ_PIPE ","WRITE_PIPE"};
+
+int show_pipe(PIPE *p)
+{
+	int i, j;
+	printf("------------ PIPE CONTENETS ------------\n");     
+	// print pipe information
+	printf("\n----------------------------------------\n");
+
+	return 1;
+}
+
+int pfd()
+{
+	// print running process' opened file descriptors
+
+	return 0;
+}
+
+int read_pipe(int fd, char *buf, int n)
+{
+	// your code for read_pipe()
+
+	return 0;
+}
+
+int write_pipe(int fd, char *buf, int n)
+{
+	// your code for write_pipe()
+
+	return 0;
+}
+
+int kpipe(int pd[2])
+{
+	// create a pipe; fill pd[0] pd[1] (in USER mode!!!) with descriptors
+
+	return 0;
+}
+
+int close_pipe(int fd)
+{
+	OFT *op; PIPE *pp;
+
+	printf("proc %d close_pipe: fd=%d\n", running->pid, fd);
+
+	op = running->fd[fd];
+	running->fd[fd] = 0;                 // clear fd[fd] entry 
+
+	if (op->mode == READ_PIPE)
+	{
+		pp = op->pipe_ptr;
+		pp->nreader--;                   // dec n reader by 1
+
+		if (--op->refCount == 0)
+		{
+			// last reader
+			if (pp->nwriter <= 0)
+			{
+				// no more writers
+				pp->busy = 0;             // free the pipe   
+				return;
+			}
+		}
+		//wakeup(&pp->room); 
+		kwakeup(&pp->room);
+		return;
+	}
+
+	if (op->mode == WRITE_PIPE)
+	{
+		pp = op->pipe_ptr;
+		pp->nwriter--;                   // dec nwriter by 1
+
+		if (--op->refCount == 0)
+		{
+			// last writer
+			if (pp->nreader <= 0)
+			{
+				// no more readers
+				pp->busy = 0;              // free pipe also 
+				return;
+			}
+		}
+
+		//wakeup(&pp->data);
+		kwakeup(&pp->data);
+		return;
+	}
+}
